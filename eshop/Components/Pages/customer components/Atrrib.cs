249a10482d553699a5735399b72ffe05d32d47db
Monther boghrara <montherhamzam.o.n.th.e.r@gmail.com>
2025-05-03 23:56:37 +0200

    first

---
+++ eshop/Components/Pages/customer components/Atrrib.cs
@@ -0,0 +1,22 @@
+﻿using eshop.Components.Account.Pages.Manage;
+
+namespace eshop.Components.Pages.customer_components
+{
+    public class Atrrib
+    {
+        public int CustomerId;
+        public string CustomerName;
+        public Email CustomerEmail;
+        public int CustomerPhone;
+        public string CustomerCity;
+
+        public Atrrib(int customerId, string customerName, Email customerEmail, int customerPhone, string customerCity)
+        {
+            this.CustomerId = customerId;
+            this.CustomerName = customerName;
+            this.CustomerEmail = customerEmail;
+            this.CustomerPhone = customerPhone;
+            this.CustomerCity = customerCity;
+        }
+    }
+}
---
+++ eshop/Components/Pages/invoice components/Invoice.cs
@@ -0,0 +1,10 @@
+﻿using eshop.Components.Pages.customer_components;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace eshop.Components.Pages.invoice_components
+{
+    public class Invoice
+    {
+        
+    }
+}
