2ac0f71372b53e18dda82e59c29dada441e985ec
Monther boghrara <montherhamzam.o.n.th.e.r@gmail.com>
2025-05-15 19:17:48 +0200

    Add InvoiceDesignServices class with CRUD methods
    Introduced a new class `InvoiceDesignServices` in the
    `eshop.Components.Pages.invoice_components` namespace.
    This class includes properties for `InvoiceId` and
    `CustomerId`, along with methods for retrieving, saving,
    updating, and deleting invoices. The retrieval methods
    return hardcoded instances for demonstration purposes.

---
+++ eshop/Components/Pages/invoice components/InvoiceDesignServices.cs
@@ -0,0 +1,45 @@
+﻿namespace eshop.Components.Pages.invoice_components
+{
+    public class InvoiceDesignServices
+    {
+        public int InvoiceId { get; private set; }
+        public int CustomerId { get; private set; }
+
+        public List <InvoiceDesignServices> GetInvoice()
+        {
+            return new List<InvoiceDesignServices>
+            {
+                new InvoiceDesignServices()
+                {
+                    InvoiceId = 1,
+                    CustomerId = 1,
+                },
+                new InvoiceDesignServices()
+                {
+                    InvoiceId = 2,
+                    CustomerId = 2,
+                }
+            };
+        }
+        public InvoiceDesignServices GetInvoiceById(int id)
+        {
+            return new InvoiceDesignServices()
+            {
+                InvoiceId = 1,
+                CustomerId = 1,
+            };
+        }
+        public Invoice Save(Invoice invoice)
+        {
+            return invoice;
+        }
+        public Invoice UpdateInvoice(Invoice invoice)
+        {
+            return invoice;
+        }
+        public void Delete(Invoice invoice)
+        {
+            // Logic to delete the invoice
+        }   
+    }
+}
